f80e72c300426a40c4cd3fa2246089ef999ae5ae
thec6rpse <canaldoutorninja@gmail.com>
2024-08-10 19:43:18 -0300

    adding the script on bool script

--- boolean.c
+++ boolean.c
@@ -1,1 +1,14 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdbool.h>
 
+int main (void) {
+    bool keep_going = true; // Could also be `bool keep_going = 1;
+    while(keep_going){
+        printf("This will run as long as keep_going is true.\n");
+        keep_going =false;      // Could be 0
+    }
+    printf("Stopping!\n");
+    return EXIT_SUCCESS;
+
+}
